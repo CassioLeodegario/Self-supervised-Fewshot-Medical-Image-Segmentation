ab4ead0e7fe6445f24a916cd553cfd6edc1ac756
CassioLeodegario <cassio.leodegario@gmail.com>
2025-11-17 19:22:29 -0300

    upgrade version

--- data/CHAOST2/dcm_img_to_nii.sh
+++ data/CHAOST2/dcm_img_to_nii.sh
@@ -1,22 +1,39 @@
-#!bin/bash
-# Convert dicom-like images to nii files in 3D
-# This is the first step for image pre-processing
+#!/bin/bash
+# Script v5: Usando dcm2niix com flags corretas (-s y, -z y, -b n)
 
-# Feed path to the downloaded data here
-DATAPATH=./MR # please put chaos dataset training fold here which contains ground truth
-
-# Feed path to the output folder here
+DATAPATH=./MR
 OUTPATH=./niis
 
-if [ ! -d  $OUTPATH/T2SPIR ]
-then
-    mkdir $OUTPATH/T2SPIR
-fi
+# 1. Cria o diretório de saída
+mkdir -p "$OUTPATH/T2SPIR"
 
+# 2. Loop através de cada ID de paciente (sid)
 for sid in $(ls "$DATAPATH")
 do
-	dcm2nii -o "$DATAPATH/$sid/T2SPIR" "$DATAPATH/$sid/T2SPIR/DICOM_anon";
-	find "$DATAPATH/$sid/T2SPIR" -name "*.nii.gz" -exec mv {} "$OUTPATH/T2SPIR/image_$sid.nii.gz" \;
-done;
+    echo "--- Processando Paciente $sid ---"
+    
+    SRC_DIR="$DATAPATH/$sid/T2SPIR/DICOM_anon"
+    DEST_DIR="$DATAPATH/$sid/T2SPIR"
 
+    # 3. Roda dcm2niix
+    # -o : diretório de saída
+    # -s y : modo de arquivo único (CRÍTICO! Isso junta as séries em um só arquivo)
+    # -z y : forçar compressão .nii.gz (CRÍTICO!)
+    # -b n : não criar BIDS .json (mais limpo)
+    # -f %p : nomear o arquivo pelo nome do Protocolo (ex: T2SPIR.nii.gz)
+    dcm2niix -o "$DEST_DIR" -s y -z y -b n -f %p "$SRC_DIR"
 
+    # 4. Encontra o ÚNICO arquivo .nii.gz que acabamos de criar
+    FILE_TO_MOVE=$(find "$DEST_DIR" -maxdepth 1 -name "*.nii.gz")
+
+    # 5. Verifica e move
+    if [ -f "$FILE_TO_MOVE" ]; then
+        echo "Arquivo NIfTI criado: $FILE_TO_MOVE"
+        mv "$FILE_TO_MOVE" "$OUTPATH/T2SPIR/image_$sid.nii.gz"
+        echo "Movido para: $OUTPATH/T2SPIR/image_$sid.nii.gz"
+    else
+        echo "ERRO: Nenhum arquivo .nii.gz foi encontrado em $DEST_DIR para o paciente $sid"
+    fi
+done
+
+echo "--- Conversão Concluída ---"
